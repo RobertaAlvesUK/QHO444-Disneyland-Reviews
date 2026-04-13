cb3af93545f55f11b1b293b594266375d57ba5fe
RobertaAlvesUK <robertabrasil24@gmail.com>
2026-04-13 22:10:13 +0100

    Added while loop to main menu for continuous navigation

--- main.py
+++ main.py
@@ -12,11 +12,16 @@ Note:   any user input/output should be done in the module 'tui'
 import tui
 tui.display_header()
 
-choice = tui.get_menu_choice()
+while True:
+    print("")
 
-if choice == "A":
-    print("[A] View Data"),
-elif choice == "B":
-    print("[B] Visualise Data"),
-else:
-    print("[X] Exit")
+    choice = tui.get_menu_choice()
+
+    if choice == "A":
+      print("[A] View Data"),
+
+    elif choice == "B":
+      print("[B] Visualise Data"),
+
+    else:
+     break
